ef1375d63fa1a76691b1a33c3eb3c0451b47f385
Yutaka Gobo <gobo@inet88.co.jp>
2022-09-20 18:34:52 +0900

    support asciidoc

--- README.md
+++ README.md
@@ -2,28 +2,29 @@
 
 ## Mermaid plugin for HonKit
 
-Plugin for [HonKit](https://github.com/honkit/honkit) which renders [Mermaid](https://github.com/knsv/mermaid) diagrams and flow charts detected in the book markdown.
+Plugin for [HonKit](https://github.com/honkit/honkit) which renders [Mermaid](https://github.com/knsv/mermaid) diagrams and flow charts detected in the book markdown and asciidoc.
 
 ### How to install it?
 
 You can use install via **npm**:
 
-```
-$ npm install honkit-plugin-mermaid
+```sh
+npm install honkit-plugin-mermaid-md-adoc
 ```
 
 And use it for your book with in the book.json:
 
-```
+```json
 {
-  "plugins": ["mermaid"]
+  "plugins": ["mermaid-md-adoc"]
 }
 ```
 
 ### How to use it?
 
 Just put the code into fenced code block and tag it **mermaid** key word like this:
-````
+
+````text
 ```mermaid
 graph
   A --> B;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
-const mermaidRegex = /^```mermaid((.*[\r\n]+)+?)?```$/im;
+const mermaidRegexMd = /^```mermaid((.*[\r\n]+)+?)?```$/im;
+const mermaidRegexAdoc = /^\[mermaid\]\n\.\.\.\.((.*[\r\n]+)+?)?\.\.\.\.$/im;
 
 function processMermaidBlockList(page) {
   let match;
-  while ((match = mermaidRegex.exec(page.content))) {
-    const rawBlock = match[0];
-    const mermaidContent = match[1];
-    page.content = page.content.replace(
-      rawBlock,
-      '<div class="mermaid">' + mermaidContent.replace('<|--', 'class_diagram_inheritance') + '</div>'
-    );
+  if (page.type === 'markdown') {
+    while ((match = mermaidRegexMd.exec(page.content))) {
+      const rawBlock = match[0];
+      const mermaidContent = match[1];
+      page.content = page.content.replace(
+        rawBlock,
+        '<div class="mermaid">' +
+          mermaidContent.replace('<|--', 'class_diagram_inheritance') +
+          '</div>'
+      );
+    }
+  } else if (page.type === 'asciidoc') {
+    while ((match = mermaidRegexAdoc.exec(page.content))) {
+      const rawBlock = match[0];
+      const mermaidContent = match[1];
+      page.content = page.content.replace(
+        rawBlock,
+        '+++<div class="mermaid">' +
+          mermaidContent.replace('<|--', 'class_diagram_inheritance') +
+          '</div>+++'
+      );
+    }
   }
   return page;
 }
 
 module.exports = {
-  website: {
-    assets: './dist',
-    js: ['book/plugin.js']
+  book: {
+    assets: './dist/assets',
+    js: ['plugin.js', 'mermaid.min.js']
   },
   hooks: {
     'page:before': processMermaidBlockList
